fb3656ddbbc745da275815a6ee938238f6be8023
Ege Dalcan <exd251@case.edu>
2026-04-22 18:29:01 -0400

    ansatz programmed

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
 import numpy as np
-import qiskit as qk
+from qiskit.circuit import Parameter
+from qiskit.transpiler import generate_preset_pass_manager
+from qiskit import QuantumCircuit
+
+def rxx_block(qc, q1, q2):
+    qc.ry(np.pi/2, q1)
+    qc.rxx(np.pi/2, q1, q2)
+
+    qc.rx(-np.pi/2, q1)
+    qc.ry(-np.pi/2, q1)
+
+    qc.rx(-np.pi/2, q2)
+
+qc = QuantumCircuit(4)
+
+angle = Parameter("angle")
+
+theta = np.zeros(4)
+
+for i, t in enumerate(theta):
+    qc.ry(t, i)
+
+rxx_block(qc, 0, 1)
+rxx_block(qc, 1, 2)
+rxx_block(qc, 2, 3)
+rxx_block(qc, 3, 0)
+
+print(qc.draw("text"))
